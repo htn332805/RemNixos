0b29198875969468f6cf93056cc28bbc4095be56
htn332805 <96609281+htn332805@users.noreply.github.com>
2025-01-02 09:45:48 -0800

    a

---
+++ misc/setup/step_02_run_flake_configuration/flake.nix
@@ -0,0 +1,43 @@
+# 💫 https://github.com/JaKooLit 💫 #
+
+{
+  description = "KooL's NixOS-Hyprland"; 
+  	
+  inputs = {
+  	nixpkgs.url = "nixpkgs/nixos-unstable";
+	#wallust.url = "git+https://codeberg.org/explosion-mental/wallust?ref=dev";
+	hyprland.url = "github:hyprwm/Hyprland"; # hyprland development
+	distro-grub-themes.url = "github:AdisonCavani/distro-grub-themes"; 
+  	};
+
+  outputs = 
+	inputs@{ self,nixpkgs, ... }:
+    	let
+      system = "x86_64-linux";
+      host = "NixOS-Hyprland";
+      username = "alice";
+
+    pkgs = import nixpkgs {
+       	inherit system;
+       	config = {
+       	allowUnfree = true;
+       	};
+      };
+    in
+      {
+	nixosConfigurations = {
+      "${host}" = nixpkgs.lib.nixosSystem rec {
+		specialArgs = { 
+			inherit system;
+			inherit inputs;
+			inherit username;
+			inherit host;
+			};
+	   		modules = [ 
+				./hosts/${host}/config.nix 
+				inputs.distro-grub-themes.nixosModules.${system}.default
+				];
+			};
+		};
+	};
+}
---
+++ misc/setup/step_02_run_flake_configuration/hosts/NixOS-Hyprland/users.nix
@@ -0,0 +1,65 @@
+# 💫 https://github.com/JaKooLit 💫 #
+# Users - NOTE: Packages defined on this will be on current user only
+
+{ pkgs, username, ... }:
+
+let
+  inherit (import ./variables.nix) gitUsername;
+in
+{
+  users = { 
+    users."${username}" = {
+      homeMode = "755";
+      isNormalUser = true;
+      description = "${gitUsername}";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+        "libvirtd"
+        "scanner"
+        "lp"
+        "video" 
+        "input" 
+        "audio"
+      ];
+
+    # define user packages here
+    packages = with pkgs; [
+      ];
+    };
+    
+    defaultUserShell = pkgs.zsh;
+  }; 
+  
+  environment.shells = with pkgs; [ zsh ];
+  environment.systemPackages = with pkgs; [ fzf ]; 
+    
+  programs = {
+  # Zsh configuration
+	  zsh = {
+    	enable = true;
+	  	enableCompletion = true;
+      ohMyZsh = {
+        enable = true;
+        plugins = ["git"];
+        theme = "xiong-chiamiov-plus"; 
+      	};
+      
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+      
+      promptInit = ''
+        fastfetch -c $HOME/.config/fastfetch/config-compact.jsonc
+        
+        #pokemon colorscripts like. Make sure to install krabby package
+        #krabby random --no-mega --no-gmax --no-regional --no-title -s; 
+        
+        source <(fzf --zsh);
+        HISTFILE=~/.zsh_history;
+        HISTSIZE=1000000;
+        SAVEHIST=1000000;
+        setopt appendhistory;
+        '';
+      };
+   };
+}
